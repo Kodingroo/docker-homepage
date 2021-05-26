434703ae760ea314df974f436b40061644561553
Isaac Hayward <kodingroo@gmail.com>
2021-05-26 11:35:52 +0900

    Adding React App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,35 +1,12 @@
-import {
-    ApolloClient,
-    ApolloProvider,
-    HttpLink,
-    InMemoryCache,
-  } from '@apollo/client';
-  import { AppProvider, EmptyState, Page } from '@shopify/polaris';
-  import { authenticatedFetch } from '@shopify/app-bridge-utils';
-  
-  import enTranslations from '@shopify/polaris/locales/en.json';
-  import React from 'react';
-  import TestData from './TestData'
-  
-  export default function App() {
-    const client = new ApolloClient({
-      link: new HttpLink({
-        credentials: 'same-origin',
-        fetch: authenticatedFetch(window.app), // created in shopify_app.js
-        uri: '/graphql'
-      }),
-      cache: new InMemoryCache()
-    });
-  
+import React from "react"
+import PropTypes from "prop-types"
+class App extends React.Component {
+  render () {
     return (
-      <AppProvider i18n={enTranslations}>
-        <ApolloProvider client={client}>
-          <Page>
-            <EmptyState>
-                <TestData/> 
-            </EmptyState>
-          </Page>
-        </ApolloProvider>
-      </AppProvider>
+      <React.Fragment>
+      </React.Fragment>
     );
   }
+}
+
+export default App
