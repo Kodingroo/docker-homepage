33f8506e66e81774bdbfc6c021d5774ee45f8169
Isaac Hayward <kodingroo@gmail.com>
2021-06-10 16:28:15 +0900

    First graphQL mutation

--- app/graphql/mutations/create_product.rb
+++ app/graphql/mutations/create_product.rb
@@ -2,17 +2,15 @@ module Mutations
     class CreateProduct < BaseMutation
       # arguments passed to the `resolve` method
       argument :title, String, required: true
-      argument :description, String
-      argument :totalInventory, Integer, required: true
+      argument :description, String, required: true
 
       # return type from the mutation
       type Types::ProductType
 
-      def resolve(title: nil, description: nil, totalInventory: nil)
+      def resolve(title: nil, description: nil)
         Product.create!(
           title: title,
           description: description,
-          total_inventory: totalInventory,
         )
       end
     end
